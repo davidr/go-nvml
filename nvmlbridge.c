d28f72f93bb88dde2272333937c3537c73a09c2a
David Ressman <davidr@ressman.org>
2016-05-01 22:25:07 -0500

    Change C comments to the .h file (from the .c)
    This commit resolves #1

--- nvmlbridge.c
+++ nvmlbridge.c
@@ -2,11 +2,6 @@
 #include "nvmlbridge.h"
 #include <stdlib.h>
 
-// Not every function can be genericized in this way because of all the custom structs,
-// but there are several nvmlGet functions we want that take a nvmlDevice_t, *char, and
-// a length as arguments. These are trivial to pass as function pointers along with their,
-// arguments, so we might as well save some effort.
-//
 int bridge_get_text_property(gettextProperty f,
                              nvmlDevice_t device,
                              char *buf,
@@ -23,7 +18,6 @@ int bridge_get_text_property(gettextProperty f,
     }
 }
 
-// Same as above, but for integer properties
 int bridge_get_int_property(getintProperty f,
                              nvmlDevice_t device,
                              unsigned int *property)
--- nvmlbridge.h
+++ nvmlbridge.h
@@ -4,7 +4,6 @@
 // but there are several nvmlGet functions we want that take a nvmlDevice_t, *char, and
 // a length as arguments. These are trivial to pass as function pointers along with their,
 // arguments, so we might as well save some effort.
-//
 typedef int (*gettextProperty) (nvmlDevice_t device , char *buf, unsigned int length);
 int bridge_get_text_property(gettextProperty f,
                              nvmlDevice_t device,
